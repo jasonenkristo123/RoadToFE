d2a14b5f211ba29d3e6c6de142398d08bb4bc618
jasonenkristo123 <jasonenkristo@gmail.com>
2025-11-02 22:59:47 +0700

    learn javascript

---
+++ jslearn/fundamental.js
@@ -0,0 +1,16 @@
+
+const eek = "10";
+const malas = 10;
+
+if (eek === malas) {
+    console.log("benar");
+} else {
+    console.log("salah");
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+const x = 5;
+(x % 2 == 0) ? console.log("x bilangan genap") : console.log("x bilangan ganjil") 
