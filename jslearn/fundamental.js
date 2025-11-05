b7acea55299eda4ecd30a8156269ec3ffee7c97e
jasonenkristo123 <jasonenkristo@gmail.com>
2025-11-05 22:11:01 +0700

    method

--- jslearn/fundamental.js
+++ jslearn/fundamental.js
@@ -1,26 +1,6 @@
-
-const eek = "10";
-const malas = 10;
-
-if (eek === malas) {
-    console.log("benar");
-} else {
-    console.log("salah");
+function luasLingkaran(jari) {
+    let luas = 3.14 * jari * jari;
+    return luas;
 }
-
-for (let i = 0; i < 10; i++) {
-    console.log(i);
-}
-
-const x = 5;
-(x % 2 == 0) ? console.log("x bilangan genap") : console.log("x bilangan ganjil");
-
-
-for (let i = 0; i < 10; i++) {
-    if (i % 2 == 0) {
-        console.log("i adalah angka genap");
-    } else {
-        console.log("i adalah angka ganjil")
-    }
-}
-
+console.log(luasLingkaran(7));
+console.log(luasLingkaran(10));
