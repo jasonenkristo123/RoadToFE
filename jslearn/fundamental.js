9f4369fe223dcbc472ea48373aa1f45004710b4c
jasonenkristo123 <jasonenkristo@gmail.com>
2025-11-05 22:27:54 +0700

    rekursif

--- jslearn/fundamental.js
+++ jslearn/fundamental.js
@@ -2,5 +2,13 @@ function luasLingkaran(jari) {
     let luas = 3.14 * jari * jari;
     return luas;
 }
-console.log(luasLingkaran(7));
+
+function faktorial(n) {
+    if (n <= 0) {
+        return 1;
+    } else {
+        return n * faktorial(n - 1);
+    }
+}
 console.log(luasLingkaran(10));
+console.log(faktorial(10));
