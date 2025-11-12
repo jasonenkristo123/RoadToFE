356a2365c3e299d884bc5b7cbd0148cc6b8be682
jasonenkristo123 <jasonenkristo@gmail.com>
2025-11-12 19:51:55 +0700

    awkoawk

--- jsw3school/script.js
+++ jsw3school/script.js
@@ -11,40 +11,40 @@
 // let number = BigInt(999999999999999);
 // console.log(number)
 
-// rest parameter
-function sum(...args) {
-    let sum = 0;
-    for (let arg of args) {
-        sum += arg;
-    }
-    return sum;
-}
-console.log(sum(10, 20, 30, 40, 50, 60, 87, 100, 202, 121));
-
-// arguments parameter
-function sumAll () {
-    let sum = 0;
-    for (let i = 0; i < arguments.length; i++) {
-        sum += arguments[i];
-    }
-    return sum;
-}
-console.log(sumAll(10, 20, 30, 40, 50));
-
-// arrow function
-let perkalian = (a, b) => a * b;
-console.log(perkalian(10, 20));
-
-let nama = () => {
-    return "Hello World";
-}
-console.log(nama());
-
-let pembagian = (a, b) => {
-    return a / b;
-}
-console.log(pembagian(10, 2));
-
+// // rest parameter
+// function sum(...args) {
+//     let sum = 0;
+//     for (let arg of args) {
+//         sum += arg;
+//     }
+//     return sum;
+// }
+// console.log(sum(10, 20, 30, 40, 50, 60, 87, 100, 202, 121));
+// 
+// // arguments parameter
+// function sumAll () {
+//     let sum = 0;
+//     for (let i = 0; i < arguments.length; i++) {
+//         sum += arguments[i];
+//     }
+//     return sum;
+// }
+// console.log(sumAll(10, 20, 30, 40, 50));
+// 
+// // arrow function
+// let perkalian = (a, b) => a * b;
+// console.log(perkalian(10, 20));
+// 
+// let nama = () => {
+//     return "Hello World";
+// }
+// console.log(nama());
+// 
+// let pembagian = (a, b) => {
+//     return a / b;
+// }
+// console.log(pembagian(10, 2));
+// 
 // kalau mau memakai return, harus ada curly brackets {}
 // kalau tidak mau pakai return tidak usah ada curly brackets
 
@@ -60,3 +60,23 @@ let tambah = (a, b) => {
 let kali = (a, b) => a * b;
 console.log(hitung(10, 5, tambah));
 console.log(hitung(10, 20, kali));
+// objek wajib pakai const
+const objek = {
+    nama: "Jason",
+    umur: 19,
+    posisi: "Mahasiswa",
+    pekerjaan: "Software engineering"
+};
+
+let text = objek;
+console.log(text);
+
+
+function person(first, last, age, eyecolor) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyecolor = eyecolor;
+};
+const personAPP = new person("Jason", "Enkristo", 18, "red");
+console.log(personAPP);
