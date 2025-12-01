ed03d91a284a6bdce4eed4f0aaaf96a80c4d4ea6
jasonenkristo123 <jasonenkristo@gmail.com>
2025-12-01 22:52:59 +0700

    queue

--- jsw3school/asynchronous/advance.js
+++ jsw3school/asynchronous/advance.js
@@ -27,7 +27,47 @@ function debounce(fn, ms) {
     return function(...args) {
         clearTimeout(timer);
         timer = setTimeout(() => {
-            fn(...args), ms;
-        })
+            fn(...args);
+        }, ms)
     }
 }
+
+// asyncqueue
+function asyncQueue() {
+    const queue = [];
+    let processing = false;
+
+    async function run() {
+        if (processing) return;
+        processing = true;
+
+        while (queue.length > 0) {
+            const task = queue.shift();
+            try {
+                const result = await task();
+                console.log(result);
+            } catch (e) {
+                console.log("err: " + e);
+            }
+        }
+        processing = false;
+    }
+
+    return {
+        push(taskFn) {
+            queue.push(taskFn);
+            run();
+        }
+    }
+}
+
+function job(name, ms) {
+    return new Promise(res => 
+        setTimeout(() => res(`Done: ${name}`), ms)
+    )   
+}
+
+const q = asyncQueue();
+q.push(() => job("Jason", 1000));
+q.push(() => job("ogil", 500));
+q.push(() => job("asa", 300));
