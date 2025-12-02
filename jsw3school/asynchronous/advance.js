ab8f72bad74ef38bd7e336a80d4e81d86fb45e60
jasonenkristo123 <jasonenkristo@gmail.com>
2025-12-02 22:06:50 +0700

    scheduler return promise

--- jsw3school/asynchronous/advance.js
+++ jsw3school/asynchronous/advance.js
@@ -33,47 +33,95 @@ function debounce(fn, ms) {
 }
 
 // asyncqueue
-function asyncQueue() {
-    const queue = [];
-    let processing = false;
+// function asyncQueue() {
+//     const queue = [];
+//     let processing = false;
 
-    async function run() {
-        if (processing) return;
-        processing = true;
+//     async function run() {
+//         if (processing) return;
+//         processing = true;
 
-        while (queue.length > 0) {
-            const task = queue.shift();
-            try {
-                const result = await task();
-                console.log(result);
-            } catch (e) {
-                console.log("err: " + e);
-            }
-        }
-        processing = false;
-    }
+//         while (queue.length > 0) {
+//             const task = queue.shift();
+//             try {
+//                 const result = await task();
+//                 console.log(result);
+//             } catch (e) {
+//                 console.log("err: " + e);
+//             }
+//         }
+//         processing = false;
+//     }
 
-    return {
-        push(taskFn) {
-            queue.push(taskFn);
-            run();
-        }
-    }
-}
+//     return {
+//         push(taskFn) {
+//             queue.push(taskFn);
+//             run();
+//         }
+//     }
+// }
 
-function job(name, ms) {
-    return new Promise(res => 
-        setTimeout(() => res(`Done: ${name}`), ms)
-    )   
-}
+// function job(name, ms) {
+//     return new Promise(res => 
+//         setTimeout(() => res(`Done: ${name}`), ms)
+//     )   
+// }
 
-const q = asyncQueue();
-q.push(() => job("Jason", 1000));
-q.push(() => job("ogil", 500));
-q.push(() => job("asa", 300));
+// const q = asyncQueue();
+// q.push(() => job("Jason", 1000));
+// q.push(() => job("ogil", 500));
+// q.push(() => job("asa", 300));
 
-// scheduler yaitu membatasi api per detik
-function schedule(limit) {
+// // scheduler yaitu membatasi api per detik
+// function schedule(limit) {
+//     const queue = [];
+//     let active = 0;
+
+//     async function run() {
+//         if (active >= limit) return;
+//         if (queue.length === 0) return;
+
+//         active++;
+//         const task = queue.shift();
+
+//         try {
+//             await task();
+//         } catch (e) {
+//             console.log("error: " + e);
+//         } finally {
+//             active--; // mengurangi task
+//             run(); // jalankan task selanjutnya
+//         }
+//     }
+
+//     return {
+//         add(taskfn) {
+//             queue.push(taskfn);
+//             run();
+//         }
+//     }
+// }
+
+// function jobs(name, ms) {
+//     return new Promise(res => {
+//         console.log("Start: " + name);
+//         setTimeout(() => {
+//             console.log("Done: " + name);
+//             res(); // promise selesai dan tidak mengembalikan nilai karena kita sudah console.log
+//         }, ms)
+//     })
+// }
+
+// const p = schedule(2);
+// p.add(() => jobs("A", 1000));
+// p.add(() => jobs("B", 500));
+// p.add(() => jobs("C", 800));
+// p.add(() => jobs("D", 300));
+// p.add(() => jobs("F", 500));
+
+// membuat scheduler yang mengembalikan promise
+
+function scheduler(limit) {
     const queue = [];
     let active = 0;
 
@@ -81,39 +129,44 @@ function schedule(limit) {
         if (active >= limit) return;
         if (queue.length === 0) return;
 
-        active++;
-        const task = queue.shift();
+        const {fn, resolve, reject} = queue.shift();
 
         try {
-            await task();
+            const result = await fn();
+            console.log(result);
         } catch (e) {
-            console.log("error: " + e);
+            reject (e);
         } finally {
-            active--; // mengurangi task
-            run(); // jalankan task selanjutnya
-        }
-    }
-
-    return {
-        add(taskfn) {
-            queue.push(taskfn);
+            active--;
             run();
         }
     }
+    return {
+        add(fn) {
+            return new Promise((resolve, reject) => {
+                queue.push({ fn, resolve, reject});
+                run();
+            });
+        }
+    };
 }
 
 function jobs(name, ms) {
     return new Promise(res => {
-        console.log("Start: " + name);
-        setTimeout(() => {
-            console.log("Done: " + name);
-            res(); // promise selesai dan tidak mengembalikan nilai karena kita sudah console.log
-        }, ms)
-    })
+        console.log("Start " + name);
+        setTimeout(() => {  
+            console.log("Done " + name);
+            res(name + " selesai");
+        }, ms);
+    });
 }
 
-const p = schedule(2);
-p.add(() => jobs("A", 1000));
-p.add(() => jobs("B", 500));
-p.add(() => jobs("C", 800));
-p.add(() => jobs("D", 300));
+const p = scheduler(2);
+async function test() {
+    const resultA = await p.add(() => jobs("A", 1000));
+    console.log("Hasil A: " + resultA);
+
+    const resultB = await p.add(() => jobs("B", 500));
+    console.log("Hasil B: " + resultB)
+}
+test();
