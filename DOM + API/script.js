03b414478941caf1923e0c75f11501aaef35617f
jasonenkristo123 <jasonenkristo@gmail.com>
2025-12-03 22:28:57 +0700

    query selector

--- DOM + API/script.js
+++ DOM + API/script.js
@@ -10,3 +10,16 @@
 
 const msg = document.getElementById("msg"); // ambil elemen id msg
 msg.textContent = "WOI Mamlas"; // ganti teks
+
+// bisa juga pakai get class
+const msg1 = document.getElementsByClassName("msg"); // akan menghasilkan html collection mirip array dan harus di loop untuk semua item
+
+// bisa pakai tag name misal button
+const button = document.getElementsByTagName("button");
+
+// menggunakan query selector untuk mengambil elemen css
+const  title = document.querySelector("#title");
+
+// kalau mau semuanya
+const item = document.querySelectorAll(".item");
+// bisa ambil semua item dan di foreach loop
