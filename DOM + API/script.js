9bf885e307dadb7c8e0ff22acc0d2562260233aa
jasonenkristo123 <jasonenkristo@gmail.com>
2025-12-03 22:13:39 +0700

    dom

---
+++ DOM + API/script.js
@@ -0,0 +1,12 @@
+// terdapat element node -> p, div, button,dll
+// text node berisi tulisan 
+// comment node berisi komentar
+
+// dom bisa mengambil elemen (queryselector)
+// mengubah elemen (textconten, innerhtml)
+// mengubah style (.style)
+// menambah elemen (appenchild), menghapus elemen(remove)
+// mendengar event (addEventListener)
+
+const msg = document.getElementById("msg"); // ambil elemen id msg
+msg.textContent = "WOI Mamlas"; // ganti teks
