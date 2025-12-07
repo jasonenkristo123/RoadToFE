57458d40716dbcc0fbd6c22b9bbeb3cb32df88a6
jasonenkristo123 <jasonenkristo@gmail.com>
2025-12-07 19:29:26 +0700

    dom

--- DOM + API/script.js
+++ DOM + API/script.js
@@ -3,7 +3,7 @@
 // comment node berisi komentar
 
 // dom bisa mengambil elemen (queryselector)
-// mengubah elemen (textconten, innerhtml)
+// mengubah elemen (textcontent, innerhtml)
 // mengubah style (.style)
 // menambah elemen (appenchild), menghapus elemen(remove)
 // mendengar event (addEventListener)
@@ -18,8 +18,43 @@ const msg1 = document.getElementsByClassName("msg"); // akan menghasilkan html c
 const button = document.getElementsByTagName("button");
 
 // menggunakan query selector untuk mengambil elemen css
-const  title = document.querySelector("#title");
+const  title = document.querySelector("#");
 
 // kalau mau semuanya
 const item = document.querySelectorAll(".item");
 // bisa ambil semua item dan di foreach loop
+
+// mengubah isi elemen
+const message = document.getElementById("message");
+message.textContent = "Merry Christmas";
+
+// ini tanpa html
+// kalau pakai html
+message.innerHTML = "<b>Happy New Year</b>";
+// kalau data dari user hindari karena bisa xss
+
+// mengubah atribut
+const img = document.querySelector("img");
+img.src = "photo.png";
+// atau bisa pakai setattribute
+img.setAttribute("width", "200");
+img.removeAttribute("width");
+
+// mengubah style
+const box = document.getElementById("box");
+box.style.backgroundColor = "tomato";
+box.style.padding = "20px";
+
+
+// mengatur class
+box.classList.add("active");
+box.classList.toggle("active");
+box.classList.remove("dark");
+
+// membuat elemen baru
+const li = document.createElement("li");
+li.textContent = "item baru";
+document.querySelector("welek").appendChild(li);
+welek.prepend(li);
+
+
